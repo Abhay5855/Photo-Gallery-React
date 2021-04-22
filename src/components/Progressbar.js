bd93ecd2c6a9fa067ce3d3464c33ca7b4a806bb7
Abhay Patil <71277980+Abhay5855@users.noreply.github.com>
2021-04-22 18:15:33 +0530

    docs:Create new function component and return the progrees

--- src/components/Progressbar.js
+++ src/components/Progressbar.js
@@ -1,2 +1,8 @@
 import react from "react";
 import UseStorage from "../hooks/useStorage";
+
+const Progressbar = () => {
+  return <div className="progress_show">progress</div>;
+};
+
+export default Progressbar;
