3d3c656d3bcb3aa9b6b40c59aef4b3049284f4f1
Abhay Patil <71277980+Abhay5855@users.noreply.github.com>
2021-04-22 17:31:59 +0530

    Create:useStorage component All the method which we require to upload is put inside the use effect hook the progress, error, and asyn function to get the URL of image

--- src/hooks/useStorage.js
+++ src/hooks/useStorage.js
@@ -1,9 +1,33 @@
 import { useState, useEffect } from "react";
-import Upload from "../components/Uploadform";
+// import Upload from "../components/Uploadform";
 import { Photostorage } from "../Firebase";
 
-const [progress, setProgress] = useState(0);
-const [error, setError] = useState(null);
-const [url, setUrl] = useState(null);
+const useStorage = (file) => {
+  const [progress, setProgress] = useState(0);
+  const [error, setError] = useState(null);
+  const [url, setUrl] = useState(null);
 
-useEffect(() => {}, [file]);
+  useEffect(() => {
+    //reference
+    const storageRef = Photostorage.ref(file.name);
+    storageRef.put(file).on(
+      "state_changed",
+      (snap) => {
+        //progress
+        let percentage = (snap.bytesTransferred / snap.totalBytes) * 100;
+        setProgress(percentage);
+      },
+      (err) => {
+        setError(err);
+      },
+      async () => {
+        const url = await storageRef.getDownloadURL();
+        setUrl(url);
+      }
+    );
+  }, [file]);
+
+  return { progress, error, url };
+};
+
+export default useStorage;
