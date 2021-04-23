67019fcd535c4da53d406ff88e490782cd108ba5
Abhay Patil <71277980+Abhay5855@users.noreply.github.com>
2021-04-23 18:30:03 +0530

    docs: Use usefirestore to get collection of images

--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -1,5 +1,15 @@
+import useFirestore from "../hooks/useFirestore";
+
 const ImageGrid = () => {
-  return <div>images</div>;
+  const { docs } = useFirestore("images");
+  console.log(docs);
+
+  return <div className="images-grid">
+    {docs && docs.map(doc => (
+
+      <img src={doc.url} alt="sketches abhay" />
+    ))};
+  </div>;
 };
 
 export default ImageGrid;
