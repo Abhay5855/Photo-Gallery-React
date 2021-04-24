fa77fa6a100ad22a917c12349005249b90a11ae2
Abhay Patil <71277980+Abhay5855@users.noreply.github.com>
2021-04-24 17:41:14 +0530

    docs: Import useState hook Ustate hook to select the Image and set the selected Image and pass it as props to the Imagrid component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import "./styles.css";
 import Title from "./components/Title";
 import Upload from "./components/Uploadform";
 import ImageGrid from "./components/ImageGrid";
+import Modal from "./components/Modal";
+import { useState } from "react";
 
 export default function App() {
+  const [selectImage, setSelectImage] = useState(null);
   return (
     <div className="App">
       <Title />
 
       <Upload />
 
-      <ImageGrid />
+      <ImageGrid setSelectImage={setSelectImage} />
+
+      <Modal />
     </div>
   );
 }
