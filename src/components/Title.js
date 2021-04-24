9980769814ce06217edcfaa3ebf736636694aa7d
Abhay Patil <71277980+Abhay5855@users.noreply.github.com>
2021-04-24 22:43:57 +0530

    docs:change in  style

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,8 +1,8 @@
 const Title = () => {
   return (
     <div className="title">
-      <p>Artist-Adda</p>
-      <h1>This is my Sketch Gallery</h1>
+      <p>@ap_sketchwork</p>
+      <h1>This is my Sketch Gallery </h1>
     </div>
   );
 };
